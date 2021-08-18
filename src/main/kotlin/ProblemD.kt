ca5c0cdf6a5e5897ea440aa10c3b92e8c00b8c01
Шадрин Михаил Дмитриевич <shadrin.m.dm@sberbank.ru>
2021-08-18 15:11:24 +0300

    added Problem D solution

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,9 +1,15 @@
 class ProblemD {
 
     fun main() {
+        var dataSets: Int = readInt()
+        while (dataSets-- > 0) {
+            val clientsNumber: Int = readInt();
+            val clientsMoney: List<Long> = readListLong().sorted()
 
-        //ваша реализация в методе main
-
+            val averageMoney = clientsMoney.sum() / clientsNumber
+            val burgerPrice = clientsMoney.find { it >= averageMoney }!!
+            val outcome = clientsMoney.filter { it >= burgerPrice }.count() * burgerPrice
+            println(outcome)
+        }
     }
-
 }
