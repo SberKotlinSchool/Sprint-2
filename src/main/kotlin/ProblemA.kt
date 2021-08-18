a40e942133f3e5964e651592c9911a7f544abd35
Шадрин Михаил Дмитриевич <shadrin.m.dm@sberbank.ru>
2021-08-18 12:53:34 +0300

    added Problem A solution

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,16 @@
 class ProblemA {
 
     fun main() {
+        var requests: Int = readInt()
+        while (requests-- > 0) {
+            val params: List<Long> = readListLong()
+            val rightJumpLength: Long = params[0]
+            val leftJumpLength: Long = params[1]
+            val jumps: Long = params[2]
 
-        //ваша реализация в методе main
-
+            val finishPoint = rightJumpLength * (jumps / 2 + jumps % 2) - leftJumpLength * (jumps / 2)
+            println(finishPoint)
+        }
     }
 
 }
