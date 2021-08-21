c702e3b0be117fa15a7c1d33e77b566d5fa620d7
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2021-08-21 19:45:52 +0300

    Update ProblemA.kt

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -2,8 +2,17 @@ class ProblemA {
 
     fun main() {
 
-        //ваша реализация в методе main
+        var entryAmount: Byte = readln().toByte() // 1 <= количество запросов <= 100
+        val outputArray = ArrayList<Long>()
 
+        while (entryAmount-- > 0) {
+            val (evenJumpInc, oddJumpInc, jumpAmount) = readLine()!!.trim().split("\\s+".toRegex()).map(String::toLong)
+            val xCoordinate: Long = (evenJumpInc - oddJumpInc) * (jumpAmount / 2) + (jumpAmount % 2) * evenJumpInc
+            outputArray.add(xCoordinate)
+        }
+        for (el in outputArray) {
+            println(el.toString())
+        }
     }
 
 }
