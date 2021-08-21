1989bc80cfe56356dbc4777b395e321c3656af72
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2021-08-21 19:47:17 +0300

    Update ProblemD.kt

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,9 +1,30 @@
 class ProblemD {
 
     fun main() {
+        var entryAmount: Byte = readln().toByte() // 1 <= количество запросов <= 100
 
-        //ваша реализация в методе main
+        val outputArray = ArrayList<Long>()
 
+        while (entryAmount-- > 0) {
+            val clientAmount: Byte = readln().toByte() // 1 <= количество клиентов <= 100
+            val coinArray: List<Long> = readListLong().sorted()
+
+            var maxCostTotal: Long = 0
+
+            for (i in 0 until clientAmount) {
+                val potCostTotal = ((clientAmount - i) * coinArray[i])
+                maxCostTotal = maxOf(maxCostTotal, potCostTotal)
+            }
+            outputArray.add(maxCostTotal)
+        }
+        for (el in outputArray) {
+            println(el)
+        }
     }
+    /*
+     Алгоритм не оптимальный, так как не исключает всех остальных клиентов после изменения потенциальной максимальной
+     стоимости бургера. Возможное улучшение: введение структуры данных(например, пары(pair<n,k>)), которая учитывает
+     количество клиентов с определенной суммой монет
+  */
 
 }
