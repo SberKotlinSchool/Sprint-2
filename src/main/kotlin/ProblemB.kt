bdb19eb7b22c8b52ee2288335114080b53ec3ba4
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2021-08-21 19:46:17 +0300

    Update ProblemB.kt

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,9 +1,21 @@
 class ProblemB {
 
     fun main() {
+        var entryAmount: Int = readln().toInt()
+        val outputArray = ArrayList<String>()
 
-        //ваша реализация в методе main
+        while (entryAmount-- > 0) {
+            var ansStr = ""
+            val (strSize, letterNum) = readLine()!!.trim().split("\\s+".toRegex()).map(String::toInt)
 
+            for (i in 0 until strSize) {
+                ansStr += ('a' + i % letterNum)
+            }
+            outputArray.add(ansStr)
+        }
+        for (el in outputArray) {
+            println(el)
+        }
     }
 
 }
