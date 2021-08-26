98678221637026b2bf813342b88957fec647d490
RamRun-Ram <64801287+RamRun-Ram@users.noreply.github.com>
2021-08-27 00:36:53 +0300

    Update Reader.kt

--- src/main/kotlin/Reader.kt
+++ src/main/kotlin/Reader.kt
@@ -12,7 +12,7 @@ fun readListInt() = readStrings().map { it.toInt() }
 fun readListLong() = readStrings().map { it.toLong() }
 
 fun problemA() {
-//  Введите колличество циклов
+//  Введите количество циклов
     var numberOfRequests = readLine()!!.toInt()
 
     for (i in 1..numberOfRequests) {
@@ -30,10 +30,10 @@ fun problemA() {
     }
 }
 fun problemB() {
-//    Введите колличество циклов
+//    Введите количество циклов
     var numberOfRequests = readLine()!!.toInt()
     repeat(numberOfRequests){
-//    Введите два числа длину строки и колличество символов через пробел
+//    Введите два числа длину строки и количество символов через пробел
         val twoValue = readLine()!!.trim().split(" ").run { IntArray(2) { get(it).toInt() } }
         for (i in 0..twoValue[0]) {
             val charCode= 97 + (i % twoValue[1])
@@ -45,13 +45,13 @@ fun problemB() {
 }
 
 fun problemC() {
-//    Введите колличество студентов
+//    Введите количество студентов
     var students = readLine()!!.toInt()
 //    Введите для каждого студента значение умения программировать через пробел
     val programmingSkills = readLine()!!.trim().split(" ").run { IntArray(students) { get(it).toInt() } }
     programmingSkills.sort()
     var miniNumberOfTasks = 0
-//    Вычисляем минимальное количество задач,для формирования команд
+//    Вычисляем минимальное количество задач для формирования команд
     for (i in 0..programmingSkills.size - 2) {
         if (i % 2 != 0) continue
         miniNumberOfTasks += programmingSkills[i + 1] - programmingSkills[i]
@@ -59,14 +59,14 @@ fun problemC() {
     print(miniNumberOfTasks)
 }
 fun problemD() {
-//    Введите колличество циклов
+//    Введите количество циклов
     var numberOfRequests = readLine()!!.toInt()
 
     repeat(numberOfRequests) {
         var marginMax: Long = 0
-//    Введите колличество клиентов
+//    Введите количество клиентов
         var numberOfClients = readLine()!!.toInt()
-//    Введите колличество монет для каждого клиента через пробел
+//    Введите количество монет для каждого клиента через пробел
         val listNumberOfCoins = readLine()!!.trim().split(" ").run { LongArray(numberOfClients) { get(it).toLong() } }
         listNumberOfCoins.sortDescending()
 //    Вычисляем максимальную прибыль кафе
