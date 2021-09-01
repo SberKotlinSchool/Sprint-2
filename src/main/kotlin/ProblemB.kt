a1e787ad40111b0836c5d81c215996581710ba6c
mvkoshenkova <mvkoshenkova@domclick.ru>
2021-09-01 21:55:00 +0300

    правка задачи B

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -3,6 +3,7 @@ class ProblemB {
     fun main() {
 
         //ваша реализация в методе main
+        //output на тестовый input может быть различным!
 
     }
 
--- src/main/kotlin/Reader.kt
+++ src/main/kotlin/Reader.kt
@@ -6,6 +6,7 @@ fun readLong() = readln().toLong()
 fun readStrings() = readln().trim().split(" ")
 
 fun readIntArray(size:Int) = readStrings().run { IntArray(size) { get(it).toInt() } }
+fun readLongArray(size:Int) = readStrings().run { LongArray(size) { get(it).toLong() } }
 
 fun readListInt() = readStrings().map { it.toInt() }
 fun readListLong() = readStrings().map { it.toLong() }
