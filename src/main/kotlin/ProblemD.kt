f1f03f5a1430a1e94b810de8943086770fa32403
thofyb <bogdyich@gmail.com>
2021-09-05 20:28:41 +0300

    ProblemD solved

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,9 +1,13 @@
 class ProblemD {
-
     fun main() {
-
-        //ваша реализация в методе main
-
+        val t = readInt();
+        for (runs in 1..t) {
+            val n = readInt();
+            val arr = readLongArray(n);
+            arr.sort();
+            val avgMoney = arr.sum() / n;
+            val price = arr.find { it >= avgMoney }!!;
+            println(arr.filter { it >= price }.size * price);
+        }
     }
-
 }
