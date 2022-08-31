f2e157ae216de26ad1c0f89d4ceb68d0cbaa5cbb
Valentina <mltoreg@mail.ru>
2022-09-01 01:19:27 +0300

    Sprint 1

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,11 @@
 class ProblemA {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val countRequest = readLong()
+        for (i in 1..countRequest) {
+            val (a, b, k) = readLongArray(3)
+            println((k - k / 2) * a - k / 2 * b)
+        }
     }
 
 }
--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -4,7 +4,15 @@ class ProblemB {
 
         //ваша реализация в методе main
         //output на тестовый input может быть различным!
-
+        val countRequest = readInt()
+        for (i in 1..countRequest) {
+            var result = ""
+            val (n, k) = readIntArray(2)
+            for (j in 0 until n) {
+                result += 'a' + (j % k)
+            }
+            println(result)
+        }
     }
 
 }
