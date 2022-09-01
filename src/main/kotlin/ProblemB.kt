fdd3e4e7d39911188a0ae6c06384b80891258a81
Шимаров Павел Евгеньевич <shimarov.P.E@sberbank.ru>
2022-09-01 17:48:04 +0300

    Sprint1

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,10 +1,31 @@
 class ProblemB {
 
+    val alphabets = "abcdefghijklmnopqrstuvwxyz"
+
     fun main() {
 
         //ваша реализация в методе main
         //output на тестовый input может быть различным!
 
-    }
+        val countString = readInt()
 
+        val result = mutableListOf<String>()
+
+        for (i in 1..countString) {
+            val stringData = readIntArray(2)
+            val stringLength = stringData[0]
+            val numberDifferentLetters = stringData[1]
+
+            if (numberDifferentLetters > stringLength) throw Exception("Количество букв больше чем длинна строки")
+            else if (numberDifferentLetters == alphabets.length)
+                result.add(alphabets.padEnd(stringLength, 'a'))
+            else {
+                val stringEnd = alphabets
+                    .substringBefore(alphabets[numberDifferentLetters])
+                    .padEnd(stringLength, 'a')
+                result.add(stringEnd)
+            }
+        }
+        result.forEach { i -> println(i) }
+    }
 }
