a68c423d93a9cfd9835fc2ef84b41051ff33ddf1
Шамрай Кирилл Константинович <Shamray.K.Ko@sberbank.ru>
2022-09-03 11:38:11 +0300

    task1: Решение заданий первого спринта

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,10 +1,23 @@
 class ProblemB {
 
     fun main() {
+        val workDir = "problemB"
+        val lines = this::class.java.getResource("$workDir/input.txt")!!.readText().lines().toMutableList()
+        val count = lines.removeFirst().toInt()
 
-        //ваша реализация в методе main
-        //output на тестовый input может быть различным!
+        val alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+        for (i in 0 until count) {
+            val line = lines[i].split(" ")
+            val n = line.first().toInt()
+            val k = line.last().toInt()
+
+            val freq: Int = n / k + 1
+            val charMap = alphabet.subSequence(0, k)
+
+            var result = charMap.repeat(freq).subSequence(0, n)
+            println(result)
+        }
     }
 
 }
