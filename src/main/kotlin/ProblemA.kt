6b452cd8bb2d5d7e4e46eff3df282d5fd31a0b45
Красилова Марина Ивановна <Krasilova.M.Iv@sberbank.ru>
2022-09-03 15:28:22 +0300

    Homework 1

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,34 @@
 class ProblemA {
 
     fun main() {
-
         //ваша реализация в методе main
+        val requestCount = readInt()
+        if (requestCount >= 1 || requestCount <= 1000) {
+            for (i in 1..requestCount) {
+                val params: LongArray = readLongArray(3)
+                println(
+                    currentPosition(
+                        jumpLengthRight = params[0],
+                        jumpLengthLeft = params[1],
+                        countJumps = params[2]
+                    )
+                )
+            }
+        } else {
+            println("Введено неверное количество запросов!")
+        }
+    }
 
+    private fun currentPosition(jumpLengthRight: Long, jumpLengthLeft: Long, countJumps: Long): Long {
+        var position = 0L
+        for (i in 1..countJumps) {
+            if (i % 2 == 1L) {
+                position += jumpLengthRight
+            } else {
+                position -= jumpLengthLeft
+            }
+        }
+        return position
     }
 
 }
