a2ad8f0ebc41d9d96ff081fc8f25d973ffdbf372
Сынков Алексей Васильевич <Synkov.A.Vas@sberbank.ru>
2022-09-04 18:35:54 +0300

    Решение тестовых заданий

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,30 @@
 class ProblemA {
 
     fun main() {
-
         //ваша реализация в методе main
+        val numberOfFrogs = readInt()
+        for (i in 1..numberOfFrogs) {
+            val inputData = readIntArray(3)
+            println(frogPosition(inputData))
+        }
+    }
 
+    private fun frogPosition(inputData: IntArray): Long {
+        val rightJump: Long = inputData.get(0).toLong()
+        val leftJump: Long = inputData.get(1).toLong()
+        val numberOfJums: Int = inputData.get(2)
+        var position: Long = 0
+
+        for (i in 1..numberOfJums) {
+            if (i % 2 == 1) {
+                position += rightJump
+            } else {
+                position -= leftJump
+            }
+        }
+
+        return position
     }
 
 }
+
