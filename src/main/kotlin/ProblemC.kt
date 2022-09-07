9da2c03a590e0371ae4998aa78833e14074ff8e9
PavelPopov4Kotlin <palepopov@sberbank.ru>
2022-09-08 02:02:15 +0600

    ProblemC resolved

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,16 @@
+import java.util.stream.Collectors
+
 class ProblemC {
 
     fun main() {
-
         //ваша реализация в методе main
-
+        val cnt = readInt()
+        val skills = readListInt().sorted()
+        var result = 0
+        for (i in 1..cnt - 1 step 2) {
+            result += skills[i] - skills[i-1]
+        }
+        println(result)
     }
 
 }
