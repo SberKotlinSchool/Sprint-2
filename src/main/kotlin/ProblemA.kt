221bbdf3ac33e1d1495f04ea632c7115e436f1b2
Поповцев Павел Владимирович <Popovtsev.P.Vl@sberbank.ru>
2022-09-07 15:05:41 +0300

    ProblemA resolved

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -2,8 +2,13 @@ class ProblemA {
 
     fun main() {
 
-        //ваша реализация в методе main
+        for(rqStep in 1..readInt()) {
+            val inputData = readListLong()
+            val positiveJump = inputData.component1()
+            val negativeJump = -inputData.component2()
+            val jumpCount = inputData.component3()
+            println("${(positiveJump + negativeJump) * (jumpCount / 2) + positiveJump * (jumpCount % 2)}")
+        }
 
     }
-
 }
