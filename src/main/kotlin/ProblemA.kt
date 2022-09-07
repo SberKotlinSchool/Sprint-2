76d72f94a20c7f3155a85ebbc6333099e99e9ca1
Поповцев Павел Владимирович <Popovtsev.P.Vl@sberbank.ru>
2022-09-07 17:26:55 +0300

    ProblemA refactored. ProblemB resolved

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -3,12 +3,18 @@ class ProblemA {
     fun main() {
 
         for(rqStep in 1..readInt()) {
-            val inputData = readListLong()
-            val positiveJump = inputData.component1()
-            val negativeJump = -inputData.component2()
-            val jumpCount = inputData.component3()
+            val (positiveJump, negativeJump, jumpCount) = loadInitialData(readListLong())
             println("${(positiveJump + negativeJump) * (jumpCount / 2) + positiveJump * (jumpCount % 2)}")
         }
-
     }
+
+    @Throws(IllegalArgumentException::class)
+    fun loadInitialData(input: List<Long>): InitialData {
+        if (input.size != 3 || input.any { it < 0 }) {
+            throw IllegalArgumentException("Initial data error!")
+        }
+        return InitialData(input.component1(), -input.component2(), input.component3())
+    }
+
+    data class InitialData(val positiveJump: Long = 0, val negativeJump: Long = 0, val jumpCount: Long = 0)
 }
