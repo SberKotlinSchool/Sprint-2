00e489607fdb7ddb8fd15c40b413ac411509a804
PavelPopov4Kotlin <palepopov@sberbank.ru>
2022-09-08 01:42:19 +0600

    ProblemB resolved

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,10 +1,29 @@
+import java.lang.Integer.min
+
 class ProblemB {
 
     fun main() {
-
         //ваша реализация в методе main
         //output на тестовый input может быть различным!
+        val lines = readInt()
+        for (i in 1..lines) {
+            val request = readListInt()
+            val length = request.get(0)
+            val maxChar = request.get(1)
+            println(getLine(length, maxChar));
+        }
+    }
 
+    fun getLine(length: Int, maxChar: Int): String {
+        var result = "";
+        for (i in 0..min(length, maxChar) - 1) {
+            result += 'a' + i;
+        }
+        val resultLength = result.length;
+        if (resultLength < length) {
+            result += getLine(length - resultLength, maxChar);
+        }
+        return result;
     }
 
 }
