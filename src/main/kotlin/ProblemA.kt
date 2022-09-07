2ccd0e98777089aabdaf00809abdf58547816721
PavelPopov4Kotlin <palepopov@sberbank.ru>
2022-09-08 00:56:26 +0600

    Problem A resolved

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,16 @@
 class ProblemA {
 
     fun main() {
-
         //ваша реализация в методе main
-
+        val lines = readInt()
+        for (i in 1..lines) {
+            val data = readListInt()
+            val a = data[0].toLong()
+            val b = data[1].toLong()
+            val k = data[2]
+            val result = ((a - b) * (k / 2)) + (if (k % 2 == 0) 0 else a)
+            println(result)
+        }
     }
 
 }
