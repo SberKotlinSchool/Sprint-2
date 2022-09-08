b7a8cf714166f65dd3cb179eaeac003cdcb0e84b
Georgy K <vangog-2005@ngs.ru>
2022-09-08 21:19:39 +0300

    sprint-1: ProblemC solved

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -2,8 +2,17 @@ class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val studentsNum = readInt()
+        val array = readIntArray(studentsNum)
+        array.sort()
+        var ans = 0
+        var i = 0
 
+        while (i < studentsNum) {
+            ans += array[i + 1] - array[i]
+            i += 2
+        }
+        println(ans)
     }
 
 }
