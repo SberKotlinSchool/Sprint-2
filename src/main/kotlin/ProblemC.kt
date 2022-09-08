e55894cccaaba5e0df4954f81997a8ab5f963241
Maria Starikova <starikova.mariya.v@gmail.com>
2022-09-08 23:33:25 +0300

    Task3

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -2,7 +2,16 @@ class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val countStudents = readInt()
+        var sumTask = 0
+        val students = readIntArray(countStudents)
+        students.sort()
+        for (i in 0..students.size-2 step 2){
+            sumTask += students[i+1] - students[i]
+        }
+        println(sumTask)
+
+
 
     }
 
