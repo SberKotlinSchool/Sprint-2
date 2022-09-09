e218d6c8b70487e3ba136562304fc15d311b18cc
Maria Starikova <starikova.mariya.v@gmail.com>
2022-09-09 18:37:18 +0300

    Task4

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -2,7 +2,21 @@ class ProblemD {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val t = readInt()
+        for(i in 1..t){
+            val countClients = readInt()
+            val price = readLongArray(countClients)
+            price.sort()
+            var maxProfit = 0L
+            var maxPrice = 0L
+            for(j in 0..countClients-1) {
+                    maxPrice = price[j]*(countClients-j)
+
+                if(maxPrice>maxProfit)
+                    maxProfit = maxPrice
+            }
+            println(maxProfit)
+        }
 
     }
 
