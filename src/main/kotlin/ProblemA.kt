98dcab30013dbd7291e8c3981038133ba175aeac
Michael <michaelmv@mail.ru>
2022-09-10 07:18:23 +0300

    Michael - Решение задач спринта 2

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,25 @@
+import java.io.InputStream
+
 class ProblemA {
-
-    fun main() {
-
-        //ваша реализация в методе main
-
+    private fun getPosition(rightJumpLength: Int, leftJumpLength: Int, jumpsAmount: Int) {
+        var position = 0L
+        for (jump in 1..jumpsAmount) {
+            when (jump % 2) {
+                1 -> position += rightJumpLength
+                0 -> position -= leftJumpLength
+            }
+        }
+        println(position)
     }
 
+    fun main() {
+        val inputStream: InputStream = this::class.java.getResource("problemA/input.txt").openStream()
+        System.setIn(inputStream)
+
+        val queryAmount = readInt()
+        for (i in 1..queryAmount) {
+            val queryParams = readIntArray(3)
+            getPosition(queryParams[0], queryParams[1], queryParams[2])
+        }
+    }
 }
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,62 @@
+import java.io.InputStream
+import kotlin.math.abs
+
 class ProblemC {
+    companion object {
+        private const val maxSkill = 100
+    }
+
+    /**
+     * Поиск индекса навыка с наименьшей разницей с указанным навыком
+     */
+    private fun findNearestSkillIndex(skill: Int, studentsSkills: MutableList<Int>): Int {
+        var result = 0
+        var difference = maxSkill
+        studentsSkills.forEachIndexed { index, item ->
+            if (abs(item - skill) < difference) {
+                difference = abs(item - skill)
+                result = index
+            }
+        }
+        return result
+    }
+
+    private fun formTeam(studentsSkills: MutableList<Int>): Pair<Int, Int> {
+        val skill1 = studentsSkills[0]
+        studentsSkills.removeAt(0)
+
+        val nearestSkillIndex = findNearestSkillIndex(skill1, studentsSkills)
+        val skill2 = studentsSkills[nearestSkillIndex]
+        studentsSkills.removeAt(nearestSkillIndex)
+
+        return Pair(skill1, skill2)
+    }
+
+    /**
+     * По идее, нужна ещё константа, чтобы можно было набирать группы
+     * с произвольным количеством студентов, но в рамках этой задачи
+     * примем, что может быть только по двое в команде
+     */
+    private fun countTasksAmount(studentsSkills: MutableList<Int>): Int {
+        var result = 0
+
+        while (studentsSkills.isNotEmpty()) {
+            val team = formTeam(studentsSkills)
+            result += abs(team.first - team.second)
+        }
+
+        return result
+    }
 
     fun main() {
+        val inputStream: InputStream = this::class.java.getResource("problemC/input.txt").openStream()
+        System.setIn(inputStream)
 
-        //ваша реализация в методе main
+        val studentsAmount = readInt()
+        val studentsSkills = readIntArray(studentsAmount).toMutableList()
 
+        val tasksAmount = countTasksAmount(studentsSkills)
+        println(tasksAmount)
     }
 
 }
