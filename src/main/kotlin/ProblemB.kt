a7cdfae8debcf784b261cf099c60b60593d54d3f
Georgy K <vangog-2005@ngs.ru>
2022-09-10 12:35:45 +0300

    sprint-1: ProblemB fixed

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -8,7 +8,7 @@ class ProblemB {
             val data = readIntArray(2)
             val len = data[0]
             val lettersNum = data[1]
-            val outputStream = StringBuffer(26)
+            val outputStream = StringBuffer(len)
 
             for (j in 0 until len) {
                 outputStream.append('a' + j % lettersNum)
