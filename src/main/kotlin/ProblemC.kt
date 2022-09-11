2e0039cd3c6f63cdafffd054456a756f5fa64b56
arpineoganisian <hwoodwri@gmail.com>
2022-09-11 09:00:15 +0300

    done

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,17 @@
+import kotlin.math.abs
+
 class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        readInt()
+        val skills = readListInt().sorted()
 
+        var problems = 0
+
+        for (i in skills.indices step 2) {
+            problems += abs(skills[i] - skills[i+1])
+        }
+        println(problems)
     }
-
 }
--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -2,8 +2,20 @@ class ProblemD {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val t = readInt()
 
+        for (i in 1..t) {
+            readInt()
+            val money = readListLong()
+
+            var maxIncome : Long = 0
+
+            for (a in money) {
+                val currIncome : Long = money.filter { it >= a }.size * a
+                if (currIncome > maxIncome)
+                    maxIncome = currIncome
+            }
+            println(maxIncome)
+        }
     }
-
 }
