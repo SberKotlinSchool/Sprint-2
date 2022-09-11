f3a465e61e99f84b68f32d2edafcdebd03c9f9d1
romankulinic <kulinichrs@gmail.com>
2022-09-11 18:54:50 +0300

    Решены все задачи, кроме задачи B. Не понятно, почеум нужно выводить симолвы именно в таком порядке , как предлагается в выходном файле.

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,9 +1,20 @@
 class ProblemB {
 
     fun main() {
+        val t = readInt()
+        for (i in 1..t) {
+            val nk  = readIntArray(2)
+            val n  = nk[0]
+            val k  = nk[1]
+            val numOfEach = n / k
+            var res = ""
+            ('a'..('a'.plus(k - 1))).forEach{
+                res += it.toString().repeat(numOfEach)
+                res += if (n % k > it.minus('a')) it.toString() else ""
+            }
+            print(res)
 
-        //ваша реализация в методе main
-        //output на тестовый input может быть различным!
+        }
 
     }
 
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,14 @@
 class ProblemC {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val students = readInt()
+        val knowledge = readIntArray(students)
+        knowledge.sort()
+        var delta  = 0
+        for(i in knowledge.indices step 2) {
+            delta = delta + knowledge[i + 1] - knowledge[i]
+        }
+        println(delta)
     }
 
 }
