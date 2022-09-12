336805f75236496b4301c6dfd9195eb60812f667
admitriev1992 <admitriev1992@gmail.com>
2022-09-12 10:11:11 +0300

    Update ProblemB.kt

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -5,9 +5,11 @@ class ProblemB {
         val stringL = readLongArray(2)
         var n = stringL[0]
         var k = stringL[1]
+        var k1 = k.toLong()
+        var pos1 = pos.toLong()
         var resultString = ""
         for (pos in 0 until n) {
-            resultString += Char(97 + (pos % k))
+            resultString += Char(97 + (pos1 % k1))
         }
         t = t - 1
         println(resultString)
