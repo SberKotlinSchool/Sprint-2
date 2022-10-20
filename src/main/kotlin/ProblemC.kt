72017632d6b8ccf71cabeb20d1fac25ea7029ff1
Dubovkin Maxim <dubovkin.m@yandex.ru>
2022-10-20 23:50:39 +0500

    sprint2 v.1

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,17 @@
+import kotlin.math.ceil
+
 class ProblemC {
 
     fun main() {
 
         //ваша реализация в методе main
-
+        val countRequests = readLong()
+        val listOfTask = readListLong().sorted()
+        println(ceil((
+                listOfTask[(countRequests/ 2 - 1).toInt()]
+                +  listOfTask[(countRequests/ 2).toInt()]
+                )/ 2.0)
+            .toInt())
     }
 
 }
