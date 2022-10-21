f7dc806454fd0621d6b6fa7eb18d4974aaf03235
albertincx <albertincx@gmail.com>
2022-10-21 21:27:30 +0200

    ProblemD solved

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -3,7 +3,33 @@ class ProblemD {
     fun main() {
 
         //ваша реализация в методе main
-
+        val count = readInt()
+        for (r in 1..count) {
+            val clients = readLong()
+            val moneys = readListLong().distinct().sorted().reversed().toLongArray()
+            val profit = getOptProfit(moneys, clients)
+            println(profit)
+        }
     }
 
+    private fun getOptProfit(moneys: LongArray, clients: Long): Long {
+        var res = 0L
+        var cnt = 1
+        if (moneys.size == 1) {
+            return clients * moneys[0]
+        }
+        for (m in moneys) {
+            val nextRes = m * cnt
+            val prevSum = res * (cnt - 1)
+            if (nextRes >= prevSum) {
+                res = m
+                cnt++
+            }
+            if (nextRes < prevSum) {
+                cnt--
+                break
+            }
+        }
+        return res * cnt
+    }
 }
