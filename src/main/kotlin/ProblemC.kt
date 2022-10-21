49dec07aa72ba71ecbf2f58b62a8feddb5a34795
albertincx <albertincx@gmail.com>
2022-10-21 19:46:48 +0200

    ProblemC solved

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,14 @@
+
 class ProblemC {
 
     fun main() {
 
         //ваша реализация в методе main
-
+        var totalTaskNum = 0
+        readInt()
+        val allStudents = readListInt().sorted()
+        for (s in 1..allStudents.size step 2)
+            totalTaskNum += allStudents[s] - allStudents[s - 1]
+        println(totalTaskNum)
     }
-
 }
