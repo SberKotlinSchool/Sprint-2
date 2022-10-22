7bc1d7ab4b74b6544eab758e1f444c7a25ede078
Aleksey Sypchenko <taler2002@mail.ru>
2022-10-22 20:31:27 +0300

    sprint-2 tasks

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,13 @@
 class ProblemC {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val size = readInt()
+        val levels = readIntArray(size)
+        levels.sort()
+        var minTasks = 0
+        for (i in 0 until size step 2) {
+            minTasks += levels[i + 1] - levels[i]
+        }
+        print(minTasks)
     }
-
 }
