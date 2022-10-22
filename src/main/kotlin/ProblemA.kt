716cc1fd061e470ca340fb4f47b875323a210386
Korshik <dakorshik@sberbank.ru>
2022-10-22 12:44:36 +0300

    Change for loop to repeat method in ProblemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -2,7 +2,7 @@ class ProblemA {
 
     fun main() {
         val t = readInt()
-        for (count in 1..t) {
+        repeat(t) {
             val params = readLongArray(3)
             val forward= ((params[2] + 1) / 2 * params[0])
             val backward = (params[2] / 2 * params[1])
