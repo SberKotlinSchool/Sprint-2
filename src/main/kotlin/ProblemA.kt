78a119ba8b520d5bcecd397135edb184f5809e29
albertincx <albertincx@gmail.com>
2022-10-22 17:22:14 +0200

    ProblemA refactoring

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,8 +1,8 @@
 class ProblemA {
 
     fun main() {
-        val requestCount = readLong()
-        for (s in 1..requestCount) {
+        val requestCount = readInt()
+        repeat(requestCount) {
             val args = readListLong().toLongArray()
             if (args.size == 3) {
                 println(startJumping(*args))
@@ -11,20 +11,12 @@ class ProblemA {
     }
 
     private fun startJumping(vararg params: Long): Long {
-        val (right, left, jumpCount) = params
-        var jump = 0L
-        var frogPos = 0L
+        val (right, left, jumps) = params
         if (left == right) {
-            return if (jumpCount % 2L == 0L) 0 else right
+            return if (jumps % 2L == 0L) 0 else right
         }
-        while (jump < jumpCount) {
-            if (jump % 2L == 0L) {
-                frogPos += right
-            } else {
-                frogPos -= left
-            }
-            jump++
-        }
-        return frogPos
+        return LongArray(jumps.toInt()) {
+            if (it % 2L == 0L) right else -left
+        }.sum()
     }
 }
