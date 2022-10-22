f97ac703692261138c862f6c7a8cc3e78335d42e
Korshik <dakorshik@sberbank.ru>
2022-10-22 13:10:16 +0300

    Use scope function in ProblemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -3,10 +3,11 @@ class ProblemA {
     fun main() {
         val t = readInt()
         repeat(t) {
-            val params = readLongArray(3)
-            val forward= ((params[2] + 1) / 2 * params[0])
-            val backward = (params[2] / 2 * params[1])
-            println(forward - backward)
+            readLongArray(3).let {
+                val forward= ((it[2] + 1) / 2 * it[0])
+                val backward = (it[2] / 2 * it[1])
+                println(forward - backward)
+            }
         }
 
     }
