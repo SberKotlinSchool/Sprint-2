0bc6d4560d1e7af70db431d220fd05b241b1297f
albertincx <albertincx@gmail.com>
2022-10-22 17:26:04 +0200

    Revert "ProblemA refactoring"
    This reverts commit 78a119ba8b520d5bcecd397135edb184f5809e29.

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,8 +1,8 @@
 class ProblemA {
 
     fun main() {
-        val requestCount = readInt()
-        repeat(requestCount) {
+        val requestCount = readLong()
+        for (s in 1..requestCount) {
             val args = readListLong().toLongArray()
             if (args.size == 3) {
                 println(startJumping(*args))
@@ -11,12 +11,20 @@ class ProblemA {
     }
 
     private fun startJumping(vararg params: Long): Long {
-        val (right, left, jumps) = params
+        val (right, left, jumpCount) = params
+        var jump = 0L
+        var frogPos = 0L
         if (left == right) {
-            return if (jumps % 2L == 0L) 0 else right
+            return if (jumpCount % 2L == 0L) 0 else right
         }
-        return LongArray(jumps.toInt()) {
-            if (it % 2L == 0L) right else -left
-        }.sum()
+        while (jump < jumpCount) {
+            if (jump % 2L == 0L) {
+                frogPos += right
+            } else {
+                frogPos -= left
+            }
+            jump++
+        }
+        return frogPos
     }
 }
