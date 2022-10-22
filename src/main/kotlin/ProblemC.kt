cfaba7414694fa56d1fb5537234a6ff0aa1cf444
Korshik <dakorshik@sberbank.ru>
2022-10-22 17:15:49 +0300

    Solve ProblemC

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,15 @@
+import kotlin.math.abs
+
 class ProblemC {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val n = readInt()
+        val st = readIntArray(n)
+            .sorted()
+            .chunked(2) { (first, second) -> first to second }
+            .filter { it.first != it.second }
+            .sumOf { abs(it.first - it.second) }
+        println(st)
     }
 
 }
