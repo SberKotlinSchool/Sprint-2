c015439e0849f05926311ec2bfc7daca192fb43a
Anna Popova <anna.popova201909@gmail.com>
2022-10-24 00:28:46 +0500

    ressolve problem B

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -4,13 +4,13 @@ class ProblemA {
         repeat(t) {
             var x: Long = 0
             val array = readLongArray(3)
-            val a : Long= array[0]
+            val a: Long = array[0]
             val b = array[1]
-            val k : Long= array[2]
+            val k: Long = array[2]
 
             val isAEven: Boolean = k.rem(2).toInt() == 0
 
-            repeat(k .div(2).toInt()) {
+            repeat(k.div(2).toInt()) {
                 x += a
                 x -= b
             }
--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,10 +1,26 @@
 class ProblemB {
 
     fun main() {
+        val alphabet = ('a'..'z').toMutableList()
 
-        //ваша реализация в методе main
-        //output на тестовый input может быть различным!
+        val t = readInt()
+        repeat(t) {
+            val array = readIntArray(2)
+            val n = array[0]
+            val k = array[1]
 
+
+            val builder = StringBuilder()
+            while (builder.count() < n) {
+                for (i in 0..k - 1) {
+                    if (builder.count() < n) {
+                        builder.append(alphabet[i])
+                    }
+                }
+
+            }
+            println(builder.toString())
+
+        }
     }
-
 }
