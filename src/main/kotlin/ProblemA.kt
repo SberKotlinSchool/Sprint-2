2f1e99ab122e5c7eb11d3f9c769477782141aeb5
Панченко Вадим Андреевич <19816992@cab-wsm-0027036.sigma.sbrf.ru>
2022-10-23 14:01:59 +0300

    Sprint 2: exercises

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,15 @@
 class ProblemA {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val frogNum = readInt()
+        for (i in 0 until frogNum) {
+            val (rightJumpLength, leftJumpLength, jumpNumber) = readLongArray(3)
+            var frogCoordinates = 0L
+            for (jump in 0 until jumpNumber) {
+                if (jump % 2 == 0L) { frogCoordinates += rightJumpLength } else { frogCoordinates -= leftJumpLength }
+            }
+            println(frogCoordinates)
+        }
     }
 
 }
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,13 @@
 class ProblemC {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val ignored = readInt() // на самом деле не нужна
+        val ratingsList = readListInt().sorted()
+        var tasksToDo = 0
+        for (i in ratingsList.indices step 2) {
+            tasksToDo += ratingsList[i+1] - ratingsList[i]
+        }
+        println(tasksToDo)
     }
 
 }
