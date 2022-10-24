d74e2911e91c6c676e0690f5e2eed6364637b7f8
Павленко Максим <m.pavlenko1991@yandex.ru>
2022-10-24 18:46:16 +0300

    Сделана задача D, крайняя версия

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -11,56 +11,16 @@ class ProblemD {
         val best_from_min = list_need.filter { s: String -> s.toLong() <= minPrice}.count()
         val best_from_avg = list_need.filter { s: String -> s.toLong() >= avgPrice}.count()
 
-        /*
-        println("------------------------------------")
-        println("MAX - $maxPrice")
-        println("MIN - $minPrice")
-        println("AVG - $avgPrice")
-
-        println("MAX_COUNT - $best_from_max")
-        println("MIN_COUNT - $best_from_min")
-        println("AVG_COUNT - $best_from_avg")
-
-         */
         val result_1 = listOf(best_from_max * maxPrice, best_from_min * minPrice, best_from_avg * avgPrice).maxOf { it }.toLong()
 
         if (result_1 == best_from_avg * avgPrice) {
 
             val minPrice_2 = list_need.filter { s: String -> s.toLong() >= avgPrice}.minOf { it.toLong() } //Минимальная цена с учётом медианы
 
-                //println("Есть совпадение со средней - $minPrice_2")
             val result_2 = listOf(best_from_max * maxPrice, best_from_min * minPrice, best_from_avg * avgPrice, best_from_avg * minPrice_2 ).maxOf { it }.toLong()
             println(result_2)
         }
         else {println(result_1)}
-
-        //println(result)
-        /*
-        print("-------------------------------------------\n")
-        println("MAX - $best_from_max")
-        println("MIN - $best_from_min")
-        println("AVG_BEST - $best_from_avg")
-        println("AVG - $avgPrice")
-
-
-
-
-
-        val resultDict: HashMap<String,List<Long>> = hashMapOf(
-                                                "MAX" to listOf<Long>(best_from_max * maxPrice, maxPrice),
-                                                "MIN" to listOf<Long>(best_from_min * minPrice, minPrice),
-                                                "AVG" to listOf<Long>(best_from_avg * avgPrice, avgPrice))
-
-
-
-
-
-        //val result = resultDict.get(listOf(best_from_max * maxPrice, best_from_min * minPrice, best_from_avg * avgPrice).maxOf { it }.toLong())
-
-
-        println(resultDict)
-
-         */
 
     }
 
