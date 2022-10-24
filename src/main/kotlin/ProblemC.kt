6c1dddcb1feec371420cc92ce1a5e5113b84caea
Павленко Максим <m.pavlenko1991@yandex.ru>
2022-10-24 14:10:38 +0300

    Сделана задача C

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,20 @@
 class ProblemC {
 
     fun main() {
-
         //ваша реализация в методе main
 
+        val listtolist = input_dir_in_list(workdir = "problemC")// input.txt
+        val sortedList = listtolist.get(0).sortedWith(compareBy({ it.toInt() }))
+        val outputlist = sortedList.withIndex().groupBy { it.index / 2 }.map { it.value.map { it.value } }
+        var result: Int = 0
+        for (lists in outputlist) {
+            result += (lists.get(1).toInt() - lists.get(0).toInt())
+        }
+        println(result)
     }
 
+}
+
+fun main(args: Array<String>) {
+    ProblemC().main()
 }
