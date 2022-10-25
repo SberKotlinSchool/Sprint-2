d77a43a9ebbee1713fef8ccbfdbc4b5d5a7b5aa6
Anna Popova <anna.popova201909@gmail.com>
2022-10-25 22:55:37 +0500

    ressolve problem C

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,13 @@
 class ProblemC {
-
+    // 2 3 5 5  10 14
     fun main() {
-
-        //ваша реализация в методе main
-
+        val n = readInt()
+        val array = readIntArray(n)
+        var count = 0
+        val sortedArray = array.sortedArray()
+        for (i in sortedArray.size - 1 downTo 0 step 2) {
+            count += sortedArray[i] - sortedArray[i - 1]
+        }
+        println(count)
     }
-
 }
