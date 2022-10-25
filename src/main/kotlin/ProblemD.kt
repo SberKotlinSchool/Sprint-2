c6cf440a3b4f8a72ed04fa61e5b974c6f00aa715
evgenypopov <evgenypopov@inbox.ru>
2022-10-25 21:29:11 +0300

    Sprint-2 homework

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -3,7 +3,24 @@ class ProblemD {
     fun main() {
 
         //ваша реализация в методе main
+        val inputStream = this::class.java.getResource("problemD/input.txt")!!.openStream()
+        System.setIn(inputStream)
 
+        for (caseCounter in 1..readInt()) {
+            val coinsOwners = readLongArray(readInt())
+            coinsOwners.sortDescending()
+            var maxProfit = 0L
+
+            for (coinsOwnerIndex in coinsOwners.indices) {
+                val profit = coinsOwners[coinsOwnerIndex] * (coinsOwnerIndex + 1)
+                if (profit > maxProfit) {
+                    maxProfit = profit
+                } else if (profit < maxProfit) {
+                    break
+                }
+            }
+            println(maxProfit)
+        }
     }
 
 }
