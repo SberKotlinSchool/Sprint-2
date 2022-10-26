85599d364e3ca18600ddc0fa5362eec4b7708e1f
evgenypopov <evgenypopov@inbox.ru>
2022-10-26 23:39:01 +0300

    Sprint-2 homework рефакторинг + подсмотрел у сокурсника способ решения задачи D

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -7,31 +7,15 @@ class ProblemC {
         val inputStream = this::class.java.getResource("problemC/input.txt")!!.openStream()
         System.setIn(inputStream)
 
-        val studentsSkills = readIntArray(readInt())
-        studentsSkills.sortDescending()
-
         // можно задавть разные значения параметра numberOfTeamMembers
         // в данном случае numberOfTeamMembers = 2
-        println(countProblemNumber(studentsSkills, numberOfTeamMembers = 2))
+        println(countProblemNumber(readIntArray(readInt()).sortedArrayDescending(), numberOfTeamMembers = 2))
     }
 
     private fun countProblemNumber(studentsSkills: IntArray, numberOfTeamMembers: Int): Int {
-        var result = 0
-        if (numberOfTeamMembers <= studentsSkills.size) {   // are there enough students for at least 1 team?
-            for (teamCounter in 0 until studentsSkills.size / numberOfTeamMembers) {
-                result += countTeamProblemNumber(studentsSkills, teamCounter * numberOfTeamMembers, numberOfTeamMembers)
-            }
+        return studentsSkills.withIndex().sumOf {
+            if (it.index % numberOfTeamMembers == 0) 0
+            else studentsSkills[it.index - it.index % numberOfTeamMembers] - it.value
         }
-        return result
-    }
-
-    private fun countTeamProblemNumber(studentsSkills: IntArray, member1Index: Int, numberOfTeamMembers: Int): Int {
-        var result = 0
-        if (member1Index + numberOfTeamMembers <= studentsSkills.size) { // are there enough members for full team?
-            for (teamMemberIndex in member1Index + 1 until member1Index + numberOfTeamMembers) {
-                result += studentsSkills[member1Index] - studentsSkills[teamMemberIndex]
-            }
-        }
-        return result
     }
 }
--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -7,19 +7,7 @@ class ProblemD {
         System.setIn(inputStream)
 
         for (caseCounter in 1..readInt()) {
-            val coinsOwners = readLongArray(readInt())
-            coinsOwners.sortDescending()
-            var maxProfit = 0L
-
-            for (coinsOwnerIndex in coinsOwners.indices) {
-                val profit = coinsOwners[coinsOwnerIndex] * (coinsOwnerIndex + 1)
-                if (profit > maxProfit) {
-                    maxProfit = profit
-                } else if (profit < maxProfit) {
-                    break
-                }
-            }
-            println(maxProfit)
+            println(readLongArray(readInt()).sortedArrayDescending().withIndex().maxOf { (it.index + 1) * it.value })
         }
     }
 
