040d0ba481447e4f1c44b2c60a3b54c182f56dd3
Романова Ирина Дмитриевна <IDmRomanova@sberbank.ru>
2022-10-29 20:40:55 +0300

    resolve problems

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,13 @@
 class ProblemC {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val n = readInt()
+        val studentsQuality = readIntArray(n).sortedArrayDescending()
+        var lessons = 0
+        for (i in 1..n / 2) {
+            lessons += (studentsQuality[2 * (i - 1)] - studentsQuality[2 * i - 1])
+        }
+        println(lessons)
     }
 
 }
