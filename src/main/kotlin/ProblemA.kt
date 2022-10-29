5d6be0d427fefd3faab4f6250396e70083ab0185
Belonogov Dmitriy <Belonogoff90@mail.ru>
2022-10-29 19:27:02 +0300

    Home works Sprint-1

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,12 @@
 class ProblemA {
-
     fun main() {
-
-        //ваша реализация в методе main
-
+        val count = readInt()
+        for (i in 1..count) {
+            val abk = readLongArray(3)
+            val a = abk[0]
+            val b = abk[1]
+            val k = abk[2]
+            println((a - b) * (k / 2) + a * (k % 2))
+        }
     }
-
 }
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,25 @@
+
 class ProblemC {
-
     fun main() {
-
-        //ваша реализация в методе main
-
+        val count = readInt()
+        val request: IntArray = readIntArray(count)
+        request.sort()
+        var a: Int
+        var b: Int
+        var c = 0
+        for (i in 0..count - 2 step 2) {
+            a = request[i]
+            b = request[i + 1]
+            while (a != b) {
+                if (a < b) {
+                    a++
+                    c++
+                } else {
+                    b++
+                    c++
+                }
+            }
+        }
+        println(c)
     }
-
 }
