43d2e71b5b5b64c32522303e0d9f59ccde27d15e
Roman Krasnenkov <RSKrasnenkov@sberbank.ru>
2023-09-22 01:11:19 +0300

    Выполнение заданий модуля Sprint-2

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,38 @@
 class ProblemA {
 
+    /**
+     *  Прыгающие лягушки
+     */
     fun main() {
+        val frogsCount = readInt()//Число лягушек
 
-        //ваша реализация в методе main
+        var i = 0
+        while (i < frogsCount) {
+            val frogInstruction = readListLong() //Инструкции для лягушки - список из [a, b, k]
+            println(frogPosition(frogInstruction[0], frogInstruction[1], frogInstruction[2]))
+            i++
+        }
+    }
 
+    /**
+     * @param hopToRight смещение вправо по оси Ox
+     * @param hopToLeft смещение влево по оси Ox
+     * @param totalHops Общее число прыжков
+     */
+    private fun frogPosition(hopToRight: Long, hopToLeft: Long, totalHops: Long): Long {
+        var position = 0L
+        var hopsCount = 0L
+        var odd = true
+        while (hopsCount < totalHops) {
+            if (odd) {
+                position += hopToRight
+            } else {
+                position -= hopToLeft
+            }
+            hopsCount++
+            odd = !odd
+        }
+        return position
     }
 
 }
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,8 +1,31 @@
 class ProblemC {
 
+    /**
+     * Формирование команд
+     */
     fun main() {
 
-        //ваша реализация в методе main
+        val studentCount = readInt()
+        if (studentCount % 2 != 0 || studentCount == 0) {
+            throw IllegalArgumentException("Ожидается четное количество студентов")
+        }
+
+        var studentsSkills = readListInt()//Умение умений студентов выраженное в баллах
+
+        //Отсортируем студентов по умности, разобьем по парам.
+        //Тот кто в паре окажется глупее, будет решать столько задач, сколько нужно, чтобы догнать соседа
+
+        studentsSkills = studentsSkills.sortedDescending()
+        var minTaskToSolve = 0;
+        var i = 0
+        while (i < studentCount) {
+            if (studentsSkills[i] > studentsSkills[i + 1]) {
+                minTaskToSolve += studentsSkills[i] - studentsSkills[i + 1]
+            }
+
+            i += 2
+        }
+        println(minTaskToSolve)
 
     }
 
