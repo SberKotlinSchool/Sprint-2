6f3a42b8d02b46aeb6a9c7b74b8c8278b9a83b75
17748568 <Rogozhin.A.Y@sberbank.ru>
2023-09-22 00:08:45 +0300

    problemB

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -2,9 +2,8 @@ import kotlin.random.Random
 
 class ProblemB {
     val abc = "abcdefghijklmnopqrstuvwxyz"
+
     fun main() {
-        val inputStream = this::class.java.getResource("problemB/input.txt")!!.openStream()
-        System.setIn(inputStream)
         var steps = readInt()
         while (steps-- > 0) {
             val line = readListInt().toMutableList()
@@ -16,8 +15,4 @@ class ProblemB {
             println(string)
         }
     }
-}
-
-fun main() {
-    ProblemB().main()
 }
