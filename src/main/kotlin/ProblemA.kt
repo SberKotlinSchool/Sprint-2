cdff9c9809630d38dfd13dd7ecc3650cf85558d8
17748568 <Rogozhin.A.Y@sberbank.ru>
2023-09-21 23:10:15 +0300

    problemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,28 @@
 class ProblemA {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val inputStream = this::class.java.getResource("problemA/input.txt")!!.openStream()
+        System.setIn(inputStream)
+        var steps = readInt()
+        while (steps-- > 0) {
+            val line = readListLong().toMutableList()
+            while (line[2] > 10) {
+                line[2] = line[2] - 10
+            }
+            var res = 0L
+            var flag = true
+            while (line[2]-- > 0) {
+                when (flag) {
+                    true -> {
+                        res += line[0]
+                    }
+                    false -> {
+                        res -= line[1]
+                    }
+                }
+                flag = !flag
+            }
+            println(res)
+        }
     }
-
 }
