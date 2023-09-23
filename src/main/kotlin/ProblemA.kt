923317e33505581f13c85508d3068b010552220c
ShedowSith <rahimyanov_aleks@mail.ru>
2023-09-23 15:41:44 +0300

    Решение первого домашнего задания

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,18 @@
 class ProblemA {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        val countRequest = readInt()
+        repeat(countRequest) {
+            val data = readListInt()
+            var x = 0L;
+            val a = data[0]
+            val b = data[1]
+            repeat(data[2]) {
+                if (it % 2 == 0) x += a
+                else x -= b
+            }
+            println(x)
+        }
     }
 
 }
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,8 +1,31 @@
+import kotlin.math.abs
+
 class ProblemC {
 
     fun main() {
+        readInt()
+        val data = readListInt().toMutableList()
+        var countTasks = 0
+        while (data.isNotEmpty()) {
+            var minCountTask: Int? = null
+            var secondStudentIndex = 0
+            val tasksFirstStudent = data.first()
+            data.removeFirst()
 
-        //ваша реализация в методе main
+            data.forEachIndexed { index, value ->
+                val difference = abs(tasksFirstStudent - value)
+                minCountTask = minCountTask?.run {
+                    if (this > difference ) {
+                        secondStudentIndex = index
+                        difference
+                    }
+                    else this
+                } ?: difference
+            }
+            data.removeAt(secondStudentIndex)
+            countTasks += minCountTask?:0
+        }
+        println(countTasks)
 
     }
 
