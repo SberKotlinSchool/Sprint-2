2366b538e3dd19967e1a731f03a37ab8811d53e3
Федор Филиппов <fefilippov@edu.hse.ru>
2023-09-24 19:17:22 +0300

    problem a done

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,24 @@
 class ProblemA {
-
     fun main() {
+        val requests = readInt()
+        val resArray = LongArray(requests)
 
-        //ваша реализация в методе main
+        for (i in 0 until requests) {
+            val row = readLongArray(3)
 
+            var current = 0L
+
+            for (j in 0 until row[2]) {
+                if (j % 2 == 0L) {
+                    current += row[0]
+                } else {
+                    current -= row[1]
+                }
+            }
+
+            resArray[i] = current
+        }
+
+        resArray.forEach { x -> println(x) }
     }
-
 }
