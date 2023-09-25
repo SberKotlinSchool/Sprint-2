488c464a89453a745ecd9590741cb33c10d3429f
popla <poplavski79@gmail.com>
2023-09-26 00:08:43 +0700

    Sprint 1. Домашнее задание. Поплавский

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -2,8 +2,18 @@ class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val count = readInt() / 2
+
+        val prog = readListLong().sorted()
+        var j : Long = 0L
+        for ( i in 1 .. count ){
+
+            j += prog[ i * 2 - 1 ] - prog[ i * 2 - 2]
+
+        }
+
+        println(j)
+
+        }
 
     }
-
-}
--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -2,8 +2,27 @@ class ProblemD {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val count = readInt()
 
-    }
+        repeat(count) {
+            var summTotal: Long = 0L
+            val peoples = readInt() - 1
+            val money = readListLong().sorted()
+
+            for ( people in 0 .. peoples  ){
+                var summ : Long = 0L
+                 for ( dollars in money){
+                     if ( dollars < money[people] )  continue
+
+                     summ += money[people]
+
+                     if ( summ > summTotal ) summTotal = summ
+
+            }
+            }
+
+            println(summTotal)
+            }
+        }
 
 }
