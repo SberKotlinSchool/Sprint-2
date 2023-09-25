80eb96e60548266d7a5996049227b27fb439dc0d
17748568 <Rogozhin.A.Y@sberbank.ru>
2023-09-25 18:39:41 +0300

    problemD

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,9 +1,28 @@
 class ProblemD {
 
     fun main() {
-
-        //ваша реализация в методе main
-
+        var dataSet = readInt()
+        while (dataSet-- > 0) {
+            readInt()
+            val visitorsCash = readListLong()
+            println(getMaxCost(visitorsCash.sorted()))
+        }
     }
 
+}
+
+fun getMaxCost(list: List<Long>): Long {
+    var max = list.size * list[0]
+    for (i in 1 until list.size) {
+        var max2 = 0L
+        for (j in list) {
+            if (j >= list[i]) {
+                max2 += list[i]
+            }
+        }
+        if (max < max2) {
+            max = max2
+        }
+    }
+    return max
 }
