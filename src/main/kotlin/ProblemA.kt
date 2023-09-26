7f12e4043b6561eefb0d720fe1c8e4a0edf24c5b
dpafonyushkin <dpafonyushkin@domclick.ru>
2023-09-26 13:55:04 +0300

    ProblemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,8 @@
 class ProblemA {
-
     fun main() {
-
-        //ваша реализация в методе main
-
+         (1..readInt()).forEach {
+            val (a, b, k) = readListLong()
+            println(( k/2 + k%2 ) * a - k/2*b )
+        }
     }
-
 }
