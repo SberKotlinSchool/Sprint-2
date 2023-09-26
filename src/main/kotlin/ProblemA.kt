1de5d6e2420fcce4ad21e3cbb395d5f59e620be4
denisov-ie <denisov.ilia@gmail.com>
2023-09-27 02:13:11 +0300

    Задачи из Sprint2

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,26 @@
 class ProblemA {
 
     fun main() {
+        val countOfFrogs = readInt()
+        for (i in 1..countOfFrogs) {
+            val jumpParameters = readLongArray(3)
+            println(
+                resolvePosition(jumpParameters[0], jumpParameters[1], jumpParameters[2])
+            )
+        }
+    }
 
-        //ваша реализация в методе main
+    private fun resolvePosition(rightJump: Long, leftJump: Long, numberOfJumps: Long): Long {
+        var position = 0L
+        for (i in 1..numberOfJumps) {
+            if (i.mod(2) == 0) {
+                position -= leftJump
+            } else {
+                position += rightJump
+            }
+        }
 
+        return position
     }
 
 }
--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,10 +1,21 @@
 class ProblemB {
 
     fun main() {
+        val countOfStrings = readInt()
+        for (i in 1..countOfStrings) {
+            val stringParameters: IntArray = readIntArray(2)
+            println(
+                resolveString(stringParameters[0], stringParameters[1])
+            )
+        }
+    }
 
-        //ваша реализация в методе main
-        //output на тестовый input может быть различным!
-
+    private fun resolveString(length: Int, countOfSymbols: Int): String {
+        val string = StringBuilder()
+        for (i in 0 until length) {
+            string.append(Char(97 + i % countOfSymbols))
+        }
+        return string.toString()
     }
 
 }
