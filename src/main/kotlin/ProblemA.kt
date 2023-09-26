919b4289e4da26971684f35129eb93d233749973
umka25 <zveryshka25@yandex.ru>
2023-09-26 13:38:24 +0300

    sprint-1

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -2,14 +2,11 @@ class ProblemA {
 
   fun main() {
     var i = readInt()
-    while (i > 0)
-      readListLong().let {
-        val data = Triple(it[0], it[1], it[2])
-        var x = 0L
-        for (j in 0 until data.third)
-          if ((j % 2).toInt() == 0) x += data.first else x -= data.second
-        println(x)
-        i--
-      }
+    while (i > 0) {
+      val data = readListLong()
+      val a = (data[2] - 1) / 2 + 1
+      println(a * data[0] - (data[2] - a) * data[1])
+      i--
+    }
   }
 }
