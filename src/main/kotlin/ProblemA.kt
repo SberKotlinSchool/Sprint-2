4efbe3072ed0791798e2a06244745e177ba1020f
DOKL57 <daniil2705@gmail.com>
2023-09-27 07:42:28 +0300

    SBERKOTLIN-Sprint2 Resolve problemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,8 +1,18 @@
 class ProblemA {
 
     fun main() {
-
-        //ваша реализация в методе main
+        val t: Int = readInt()
+        for (i in 0 until t) {
+            val parameters: LongArray = readLongArray(3)
+            val a: Long = parameters[0]
+            val b: Long = parameters[1]
+            val k: Long = parameters[2]
+            if ((k % 2).toInt() == 0) {
+                println(a * (k / 2) - b * (k / 2))
+            } else {
+                println(a * (k / 2 + 1) - b * (k / 2))
+            }
+        }
 
     }
 
