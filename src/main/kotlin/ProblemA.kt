5ca922b3040a41dbe317f7adbe3c3294dd50bc27
avkononenko <avkononenko@domclick.ru>
2023-09-27 23:57:06 +0300

    Задания к 1 спринту

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,11 @@
 class ProblemA {
 
     fun main() {
-
         //ваша реализация в методе main
-
+        repeat(readInt()) {
+            val (a, b, k) = readLongArray(3)
+            println(a * (k / 2 + k % 2) - b * (k / 2))
+        }
     }
 
 }
--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,9 +1,15 @@
 class ProblemD {
 
     fun main() {
-
         //ваша реализация в методе main
+        repeat(readInt()) {
+            val clients = readLongArray(readInt()).sortedDescending()
+            val result = clients.mapIndexed { index, client ->
+                client * (index + 1)
+            }.maxOrNull() ?: 0
 
+            println(result)
+        }
     }
 
 }
