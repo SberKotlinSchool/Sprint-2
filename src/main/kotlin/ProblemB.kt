28145fb916e95dc63da2b0e33ef3559b29a0cd14
anton <harbinger2113@gmail.com>
2023-09-27 22:34:42 +0300

    refactoring ProblemB

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,15 +1,17 @@
 import kotlin.random.Random
 
 class ProblemB {
-    val abc = "abcdefghijklmnopqrstuvwxyz"
-
     fun main() {
         var steps = readInt()
         while (steps-- > 0) {
             val line = readListInt().toMutableList()
             var string = ""
+            var shift = 0
             while (line[0]-- > 0) {
-                val letter = abc[Random.nextInt(line[1])]
+                if (shift >= line[1]) {
+                    shift = 0
+                }
+                val letter = 'a' + shift++
                 string += letter
             }
             println(string)
