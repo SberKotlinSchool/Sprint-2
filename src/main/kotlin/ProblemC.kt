e64123a7f9538b7dcbf432d2745acb2d08d3ca50
danilovae <liza@danilova.me>
2023-09-29 21:19:01 +0300

    Первое ДЗ

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -2,8 +2,17 @@ class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val students = readInt()
+        val skills = readIntArray(students)
 
+        skills.sort()
+
+        var tasks = 0
+        for (i in 0 until skills.lastIndex step 2) {
+            tasks += skills[i + 1] - skills[i]
+        }
+
+        println(tasks)
     }
 
 }
