10ffd8dd55ad21ff37101e8c70d7f59a92f88e35
Yury Gorbunov <YVGorbunov@sberbank.ru>
2023-09-30 23:01:08 +0300

    Homework. Sprint-2

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,9 +1,28 @@
+import java.io.File
+import java.io.InputStream
+
 class ProblemA {
+    val filename = "src/main/resources/problemA/input.txt"
+    val delimiter = " "
 
     fun main() {
+        val inputStream: InputStream = File(filename).inputStream()
+        val lines = mutableListOf<String>()
+        inputStream.bufferedReader().forEachLine { lines.add(it) }
 
-        //ваша реализация в методе main
+        val linesCount = lines[0].toInt()
+        for (i in 1..linesCount) {
+            val abk = lines.get(i).split(delimiter);
+            val goRightSteps = abk[0].toLong()
+            val goLeftSteps = abk[1].toLong()
+            val repeatCount = abk[2].toLong()
+
+            var result = ((goRightSteps - goLeftSteps) * (repeatCount / 2))
+            if (repeatCount % 2 > 0)
+                result += goRightSteps
+
+            println(result)
+        }
 
     }
-
 }
--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,9 +1,37 @@
+import java.io.File
+import java.io.InputStream
+
 class ProblemB {
 
-    fun main() {
+    val ABC = "abcdefghijklmnopqrstuvwxyzabcdefgh"
+    val MAX_ABC_SYMBOLS = ABC.length
 
-        //ваша реализация в методе main
-        //output на тестовый input может быть различным!
+    val filename = "src/main/resources/problemB/input.txt"
+    val delimiter = " "
+
+    fun main() {
+        val inputStream: InputStream = File(filename).inputStream()
+        val lines = mutableListOf<String>()
+        inputStream.bufferedReader().forEachLine { lines.add(it) }
+
+        val linesCount = lines[0].toInt()
+        for (i in 1..linesCount) {
+            val abk = lines.get(i).split(delimiter);
+            val stringLength = abk[0].toInt()
+            val abcLength = abk[1].toInt()
+            val abcToUseString = ABC.substring(0, abcLength)
+
+            var result = ""
+            var abcIndex = 0;
+            for (j in 0..stringLength - 1) {
+                result += abcToUseString[abcIndex]
+                abcIndex++
+                if (abcIndex >= abcLength)
+                    abcIndex = 0
+            }
+
+            println(result)
+        }
 
     }
 
