eb0f6d358d5ba46f86714b788abe4ddd2006a82e
Ivan Muratov <strateg8888@gmail.com>
2023-10-02 12:16:22 +0400

    Home work

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,8 +1,13 @@
 class ProblemC {
 
     fun main() {
-
-        //ваша реализация в методе main
+        val stringCount = readInt()
+        var result = 0
+        val skillsStudent = readIntArray(stringCount).sorted()
+        for (i in skillsStudent.indices step 2){
+            result += skillsStudent[i + 1] - skillsStudent[i]
+        }
+        println(result)
 
     }
 
